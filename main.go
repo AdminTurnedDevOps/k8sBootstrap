96999c378a88f7928626a63ffb6469a2f238c615
michael levan <michael.levan@orbica.world>
2023-06-11 08:04:26 -0400

    kubeconfig

--- main.go
+++ main.go
@@ -3,8 +3,12 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
 	"path/filepath"
 
+	"github.com/opentracing/opentracing-go/log"
+	"k8s.io/client-go/dynamic"
+	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/client-go/util/homedir"
 )
 
@@ -25,6 +29,18 @@ func connectContext() {
 		kubeConfig = flag.String("kubeConfig", filepath.Join(home, ".kube", "config"), "")
 	}
 
-	fmt.Println(*kubeConfig)
+	kubeConfigPath, err := clientcmd.BuildConfigFromFlags("", *kubeConfig)
+	if err != nil {
+		log.Fatalln(err)
+		fmt.Print("Enter Your Kubeconfig Context: ")
+		fmt.Scan(&kubeConfig)
+	}
+
+	clientConfig, err := dynamic.NewForConfig(kubeConfigPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(clientConfig)
 
 }
