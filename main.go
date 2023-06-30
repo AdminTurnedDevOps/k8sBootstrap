310241ec216a23a3f3996e3e9cc45a7fca800b2b
michael levan <michael.levan@orbica.world>
2023-06-30 12:01:18 -0400

    argo

--- main.go
+++ main.go
@@ -4,16 +4,22 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"os"
 	"path/filepath"
 
 	"github.com/opentracing/opentracing-go/log"
 	"k8s.io/client-go/dynamic"
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/client-go/util/homedir"
+
+	"helm.sh/helm/v3/pkg/action"
+	"helm.sh/helm/v3/pkg/chart/loader"
+	"helm.sh/helm/v3/pkg/cli"
 )
 
 func main() {
 	connectContext()
+	installArgo()
 }
 
 func connectContext() {
@@ -42,5 +48,40 @@ func connectContext() {
 	}
 
 	fmt.Println(clientConfig)
+
+}
+
+func installArgo() {
+
+	helmChart := "argo/argo-cd"
+	releaseName := "argocd"
+
+	// Call upon the CLI package
+	settings := cli.New()
+
+	chart, err := loader.Load(helmChart)
+	if err != nil {
+		log.Println(err)
+	}
+
+	// Create a new instance of the configuration
+	config := new(action.Configuration)
+
+	// Collect local Helm information
+	if err := config.Init(settings.RESTClientGetter(), settings.Namespace(), os.Getenv("HELM_DRIVER"), log.Printf); err != nil {
+		log.Printf("%+v", err)
+	}
+
+	// Create a new instance of the `Install` action, which is similar to running `helm instll`
+	client := action.NewInstall(config)
+
+	// Install a helm chart
+	client.ReleaseName = releaseName
+	results, err := client.Run(chart, nil)
+	if err != nil {
+		log.Printf("%+v", err)
+	}
+
+	fmt.Println(results)
 
 }
