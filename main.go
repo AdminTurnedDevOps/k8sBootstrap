7fce52558bd560561216ac95d0e6ca93a71eadad
michael levan <michael.levan@orbica.world>
2023-07-02 19:22:30 -0400

    opentrace library removal

--- main.go
+++ main.go
@@ -7,7 +7,6 @@ import (
 	"os"
 	"path/filepath"
 
-	// "github.com/opentracing/opentracing-go/log"
 	"k8s.io/client-go/dynamic"
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/client-go/util/homedir"
